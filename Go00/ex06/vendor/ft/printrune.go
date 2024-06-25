628611c0789ec3a5f569696114df9ed4b511431b
Homma Rinka <hrinka@c1r7s8.42tokyo.jp>
2024-06-25 17:50:49 +0900

    ftchange

--- Go00/ex06/vendor/ft/printrune.go
+++ Go00/ex06/vendor/ft/printrune.go
@@ -1,7 +1,20 @@
 package ft
 
-import "fmt"
+import (
+	"errors"
+	"os"
+	"unicode/utf8"
+)
 
-func PrintRune(r rune) {
-	fmt.Printf("%c", r)
+// PrintRune prints a single rune (Unicode code point) and returns any error
+// if the encoding or the writing fails.
+func PrintRune(r rune) error {
+	l := utf8.RuneLen(r)
+	if l == -1 {
+		return errors.New("The rune is not a valid value to encode in UTF-8")
+	}
+	p := make([]byte, l)
+	utf8.EncodeRune(p, r)
+	_, err := os.Stdout.Write(p)
+	return err
 }
